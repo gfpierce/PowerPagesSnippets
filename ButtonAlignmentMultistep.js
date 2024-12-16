f61242b9cf4c26855e0f593d73da5f98bdea74b3
Garrett Pierce <31076033+gfpierce@users.noreply.github.com>
2024-12-16 10:55:44 -0500

    Added ButtonAlignmentMultistep.js

---
+++ ButtonAlignmentMultistep.js
@@ -0,0 +1,7 @@
+// Replace #NextButton with the desired button
+
+// Align right
+$(document).ready(function() {
+    $("#NextButton").parent().parent().removeClass("col-md-6").addClass("col-md");
+    $("#NextButton").parent().addClass("float-end");
+});
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PowerPagesSnippets
 Snippets of code to accomplish various tasks in Power Pages
+
+## ButtonAlignmentMultistep.js
+Code that alters the alignment of Previous and Next buttons on a multistep form
